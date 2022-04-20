6bf9ee42b20478ed031fa50af8cc13c39c45f663
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-04-20 13:00:56 +0530

    User routes imported

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const colors = require("colors")
 const dotenv = require("dotenv")
 dotenv.config();
 
+const usersRoutes = require("./routes/users")
+
 // Initialize App
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(express.json())
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connected".underline.red)).catch((error) => console.log(error))
+
+app.use("/users/", usersRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
